ddf2ba3c6be721329902f257f2a55b07aa03cdeb
Elizaveta Bolshova <bolshovaeliza@gmail.com>
2026-05-18 16:44:46 +0300

    ci(semantic-release): add breaking change support

--- .releaserc.js
+++ .releaserc.js
@@ -2,8 +2,11 @@ module.exports = {
   branches: ['main'],
 
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits'
+    }],
     ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
       writerOpts: {
         headerPartial: '',
         commitGroupsSort: (a, b) => {
